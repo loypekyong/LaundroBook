e48c34665f5e621eb3b611ef31a017aae5124c6b
loypekyong <loypekyong@gmail.com>
2023-03-04 23:16:15 +0800

    firebase test

--- app/src/main/java/com/example/laundrobook/MainActivity.java
+++ app/src/main/java/com/example/laundrobook/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.laundrobook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.Timestamp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Test firebase code start
+    TextView textViewMsg;
+    final String node = "current_msg";
+    DatabaseReference mRootDatabaseRef;
+    DatabaseReference mNodeRef;
+
+    //Test firebase code end
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //firebase test
+        textViewMsg = findViewById(R.id.textViewMsg);
+        mRootDatabaseRef = FirebaseDatabase.getInstance().getReference();
+        mNodeRef = mRootDatabaseRef.child(node);
+
+        mNodeRef.addValueEventListener(new ValueEventListener() {
+
+            // when firebase value change it runs the next method
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String change = snapshot.getValue(String.class);
+                textViewMsg.setText(change);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        Date d = new Date(System.currentTimeMillis());
+        Timestamp timestamp = new Timestamp(d);
+        mNodeRef.setValue(timestamp.toString());
+
+        // end firebase test
+
+
+
     }
 }
